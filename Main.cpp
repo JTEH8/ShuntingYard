dae7e2a12fe96b835c6188c57caedb74911570a4
JTEH8 <jeffteh22@gmail.com>
2022-03-11 10:59:40 -0800

    Really, finished with comments.

--- Main.cpp
+++ Main.cpp
@@ -8,12 +8,12 @@ Implementing queue referencing code from: https://www.geeksforgeeks.org/queue-li
 `Thanks to Jayden Huang for helping me with this project.
 */
 using namespace std;
-
+//Node Struct
 struct Node{
 int data = 0;
 Node* next;
 };
-
+//Function Prototypes
 void postfix(char* input, Node* &stack, Node* &queueFront, Node* &queueRear);
 int order(char operation);
 void pop(Node* &top);
@@ -33,34 +33,43 @@ void printPostfix(Tree* Tree);
 void printPrefix(Tree* Tree);
 
 
-
+//Main Method
 int main(){
     //Initialize the Stack and Queue
     Node* Stack = NULL;
     Node* queueFront = NULL;
     Node* queueRear = NULL;
     Tree* head = NULL;
+    //Text Prompt
     cout << "Welcome to the Shunting Yard Algorithm! Please enter an equation in infix (normal) notation. Please don't use spaces to separate characters/numbers." << endl;
     char input[100];
     int num = 0;
-    cin>>input;
+    //Entering User input
+    cin >> input;
     postfix(input,Stack,queueFront,queueRear);
+    //While the queue's front and rear aren't NULL
     while(queueFront != NULL && queueRear != NULL){
+    //If the input char is a digit
       if(isdigit(dequeue(queueFront,queueRear))){
+    //Make a branch
             Tree* branch = new Tree(dequeue(queueFront,queueRear));
             treePush(head,branch);
         }
         else{
+            //Make a branch
             Tree* branch = new Tree(dequeue(queueFront,queueRear));
             Tree* temp = NULL;
+            //Create left and right branches from that, and push it to the tree.=
             treePop(head, temp);
             branch->setRight(temp);
             treePop(head,temp);
             branch->setLeft(temp);
             treePush(head, branch);
         }
+        //Get rid of the end of the queue.
         deleteQueue(queueFront, queueRear);
         }
+    //Printing out Postfix, Infix, and Prefix
     printPostfix(head);
     cout << "Infix: ";
     printInfix(head);
@@ -72,14 +81,14 @@ int main(){
     printPrefix(head);
     exit(0);
 }
-
+//Push method
 void push(char newData, Node* &top){
 Node* temp = new Node();
 temp->data = newData;
 temp->next = top;
 top = temp;
 }
-
+//Popping off the top of the stack
 void pop(Node* &top){
 if(top == NULL){
     cout << "The stack is empty!" << endl;
@@ -92,7 +101,7 @@ else{
 
 }
 }
-
+//Popps off the top of the stack and moves it to the queue
 void popTo(Node* &top, Node* &front, Node* &rear){
     if(top == NULL){
         return;  
@@ -105,7 +114,7 @@ void popTo(Node* &top, Node* &front, Node* &rear){
         delete temp;
     }
 }
-
+//Set order of operations for operands
 int order(char operation){
 if(operation == '+' || operation == '-'){
     return 1;
@@ -123,7 +132,7 @@ else{
     return 0;
 }
 }
-
+//Returns the value at the top of the stack
 char peek(Node* top){
     if(top != NULL){
         return top->data;
@@ -132,7 +141,7 @@ char peek(Node* top){
         return ' ';
     }
 }
-
+//Add values to the queue
 void enqueue(Node* &front, Node* &rear, char newData){
     if(front == NULL && rear == NULL){
         Node* temp = new Node();
@@ -149,7 +158,7 @@ void enqueue(Node* &front, Node* &rear, char newData){
         front = temp;
     }
 }
-
+//Returns the value at the front of the queue
 char dequeue(Node* &tempFront, Node* &rear){
     if(rear == NULL){
         return 'A';
@@ -171,7 +180,7 @@ char dequeue(Node* &tempFront, Node* &rear){
         return dequeue(tempFront->next, rear);
 }
 }
-
+//Deletes the rear of the queue
 void deleteQueue(Node* &front, Node* &rear){
     if(rear == NULL){
         return;
@@ -191,9 +200,10 @@ void deleteQueue(Node* &front, Node* &rear){
         deleteQueue(front->next, rear);
     }
 }
-
+//Turn infix notation into postfix
 void postfix(char* input, Node* &stack, Node* &queueFront, Node* &queueRear){
     int i = 0; 
+    //Going through the length of the input
     while(i <= strlen(input)){
         char val = input[i];
         //If the characters is a number
@@ -201,16 +211,24 @@ void postfix(char* input, Node* &stack, Node* &queueFront, Node* &queueRear){
             //Add it to the Queue
             enqueue(queueFront,queueRear,val);
         }
+        //If the character is an operand
         else if(val == '^' || val == '*' || val == '+' || val == '-' || val == '/'){
+        //While it isn't a parentheses or until an operand with a different order is read in
           while(peek(stack) != 'A' && order(peek(stack)) != 5 && order(peek(stack)) >= order(val) && order(val) != 3){
+                //Pop it to the queue
                 popTo(stack,queueFront, queueRear);
             }
+            //Push it to the stack
              push(val, stack);
         }
+        //If it is a parentheses
         else if(val == '('){
+            //Push it to the stack
             push(val, stack);
         }
+        //If it's an end parentheses
         else if(val == ')'){
+            //Run through the in between
             bool runThrough = true;
             while(runThrough == true){
                 if(peek(stack) == '('){
@@ -218,30 +236,33 @@ void postfix(char* input, Node* &stack, Node* &queueFront, Node* &queueRear){
                     runThrough = false; 
                 }
                 else{
+                //Pop it to the stack
                 popTo(stack,queueFront, queueRear);
                 }
             }
         }
         else{
+            //While the stack isn't NULL
             while(stack != NULL){
+                //Pop it to the queue
                 popTo(stack,queueFront, queueRear);
             }
         }
         i++;
     }
 }
-
+//Pushing a new branch to the tree
 void treePush(Tree* &newTree, Tree* &top){
     Tree* temp =top;
     temp->setNext(newTree);
     newTree = temp;
 }
-
+//Popping a branch off the tree
 void treePop(Tree* &newTree, Tree* &top){
     top = newTree;
     newTree = newTree->next;
 }
-
+//Getting the top of the tree
 Tree* treePeek(Tree* top){
     if(top != NULL){
         return top;
@@ -251,8 +272,7 @@ Tree* treePeek(Tree* top){
     }
 }
 
-
-
+//Printing in infix notation
 void printInfix(Tree* Tree){
     if(Tree != NULL){
         if(isdigit(treePeek(Tree)->getData()) == false){
@@ -266,7 +286,7 @@ void printInfix(Tree* Tree){
         }
     }
 }
-
+//Printing in postfix notation
 void printPostfix(Tree* Tree){
     if(Tree == NULL){
         return;
@@ -284,7 +304,7 @@ void printPostfix(Tree* Tree){
     cout << Tree->getData();
     */
 }  
-
+//Printing in prefix notation
 void printPrefix(Tree* Tree){
     cout << Tree->getData();
     if(Tree->getLeft() != NULL){
